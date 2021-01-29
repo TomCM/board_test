13d66b33519fa6313b7d879d3e998f3b4a8e019a
chenming <abc200310348@126.com>
2021-01-29 18:05:58 +0800

    第一次

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    publicPath: '/kanban',
+    // 输出文件目录
+    outputDir: 'dist',
+    // webpack-dev-server 相关配置
+    devServer: {
+        // 本地端口
+        port: 8686,
+    },
+    productionSourceMap: false,
+}
