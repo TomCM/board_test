ad9c5dd120788227467a9647983b1e99ed644bfb
chenming <abc200310348@126.com>
2021-02-01 11:00:34 +0800

    冲突测试1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">My Board</router-link> |
+    <router-link to="/">My Board 修改 1</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
